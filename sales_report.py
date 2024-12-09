2c4d2e2391b4a63d9867b381964ca7bcafc4c32b
Junghyeongjin <hjjung@hansae.com>
2024-12-09 09:25:51 +0900

    using images URL from git hub

--- sales_report.py
+++ sales_report.py
@@ -18,7 +18,7 @@ def get_image_file_as_base64(file_path):
 st.set_page_config(layout="wide")
 
 # Correct absolute path to your GIF image
-gif_path = "data\Sales report.gif"
+gif_path = "https://github.com/Junghyeongjin/HSTGTSR/blob/main/data/Sales%20report.gif?raw=true"
 
 # Display GIF on the first row
 try:
